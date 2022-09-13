17e303b3cba57b538e354cd889789403a629646e
parsifal <crawshawh04@gmail.com>
2022-09-13 20:59:43 +0100

    Add visitor IDs and page numbers to LogProducer

--- app/src/main/scala/clickstream/LogProducer.scala
+++ app/src/main/scala/clickstream/LogProducer.scala
@@ -8,5 +8,8 @@ object LogProducer extends App {
 
   val Products = scala.io.Source.fromInputStream(getClass.getResourceAsStream("/products.csv")).getLines().toArray
   val Referrers = scala.io.Source.fromInputStream(getClass.getResourceAsStream("/referrers.csv")).getLines().toArray
-  
+  val Visitors = (0 to wlc.visitors).map("Visitor-" + _)
+  val Pages = (0 to wlc.pages).map("Page-" + _)
+
+  println(Visitors)
 }
