95b37cd09d7a3e057c3dc6f4db2019afe58879d4
parsifal <crawshawh04@gmail.com>
2022-09-16 17:53:02 +0100

    Update SparkUtils

--- app/src/main/scala/utils/SparkUtils.scala
+++ app/src/main/scala/utils/SparkUtils.scala
@@ -1,5 +1,28 @@
 package utils
 
-object SparkUtils {
+import java.lang.management.ManagementFactory
 
-}
+import org.apache.spark.sql.SQLContext
+import org.apache.spark.streaming.{StreamingContext, Duration}
+import org.apache.spark.{SparkContext, SparkConf}
+
+object SparkUtils {
+  val isIDE = {
+    ManagementFactory.getRuntimeMXBean.getInputArguments.toString.contains("IntelliJ IDEA")
+  }
+  def getSparkContext(appName: String) = {
+    var checkpointDirectory = ""
+
+    // get spark configuration
+    val conf = new SparkConf()
+      .setAppName(appName)
+      .set("spark.casandra.connection.host", "localhost")
+
+    // Check if running from IDE
+    if (isIDE) {
+      System.setProperty("hadoop.home.dir", "F:\\Libraries\\WinUtils") // required for winutils
+      conf.setMaster("local[*]")
+      checkpointDirectory = "file:///f:/temp"
+    } else {
+      checkpointDirectory = "hdfs://lambda-pluralsight:9000/spark/checkpoint"
+    }
