0aac7b9a2f6133cadc9e9e9d8997516ead562551
parsifal <crawshawh04@gmail.com>
2022-09-16 17:36:21 +0100

    Refactor LogProducer

--- app/src/main/scala/clickstream/LogProducer.scala
+++ app/src/main/scala/clickstream/LogProducer.scala
@@ -1,14 +1,14 @@
 package clickstream
 
-import config.Settings
-import org.apache.commons.io.FileUtils
+import java.util.Properties
 
-import java.io.FileWriter
+import config.Settings
+import org.apache.kafka.clients.producer.{KafkaProducer, Producer, ProducerConfig, ProducerRecord}
+
 import scala.util.Random
 
-
 object LogProducer extends App {
-
+  // WebLog config
   val wlc = Settings.WebLogGen
 
   val Products = scala.io.Source.fromInputStream(getClass.getResourceAsStream("/products.csv")).getLines().toArray
@@ -17,13 +17,24 @@ object LogProducer extends App {
   val Pages = (0 to wlc.pages).map("Page-" + _)
 
   val rnd = new Random()
-  val filePath = wlc.filePath
-  val destPath = wlc.destPath
 
-  for (fileCount <- 1 to wlc.numFiles) {
+  val topic = wlc.kafkaTopic
+  val props = new Properties()
 
-    val fw = new FileWriter(filePath, true)
+  props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092")
+  props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringSerializer")
+  props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringSerializer")
+  props.put(ProducerConfig.ACKS_CONFIG, "all")
+  props.put(ProducerConfig.CLIENT_ID_CONFIG, "WebLogProducer")
 
+  val kafkaProducer: Producer[Nothing, String] = new KafkaProducer[Nothing, String](props)
+  println(kafkaProducer.partitionsFor(topic))
+
+  for (fileCount <- 1 to wlc.numberOfFiles) {
+
+    //val fw = new FileWriter(filePath, true)
+
+    // introduce some randomness to time increments for demo purposes
     val incrementTimeEvery = rnd.nextInt(wlc.records - 1) + 1
 
     var timestamp = System.currentTimeMillis()
@@ -31,7 +42,7 @@ object LogProducer extends App {
 
     for (iteration <- 1 to wlc.records) {
       adjustedTimestamp = adjustedTimestamp + ((System.currentTimeMillis() - timestamp) * wlc.timeMultiplier)
-      timestamp = System.currentTimeMillis()
+      timestamp = System.currentTimeMillis() // move all this to a function
       val action = iteration % (rnd.nextInt(200) + 1) match {
         case 0 => "purchase"
         case 1 => "add_to_cart"
@@ -47,7 +58,9 @@ object LogProducer extends App {
       val product = Products(rnd.nextInt(Products.length - 1))
 
       val line = s"$adjustedTimestamp\t$referrer\t$action\t$prevPage\t$visitor\t$page\t$product\n"
-      fw.write(line)
+      val producerRecord = new ProducerRecord(topic, line)
+      kafkaProducer.send(producerRecord)
+      //fw.write(line)
 
       if (iteration % incrementTimeEvery == 0) {
         println(s"Sent $iteration messages!")
@@ -55,13 +68,12 @@ object LogProducer extends App {
         println(s"Sleeping for $sleeping ms")
         Thread sleep sleeping
       }
-    }
-    fw.close()
 
-    val outputFile = FileUtils.getFile(s"${destPath}data_$timestamp")
-    println(s"Moving produced data to $destPath")
-    FileUtils.moveFile(FileUtils.getFile(filePath), outputFile)
-    val sleeping = 5000
-    println(s"Sleeping for $sleeping ms...")
+    }
+
+    val sleeping = 2000
+    println(s"Sleeping for $sleeping ms")
   }
+
+  kafkaProducer.close()
 }
--- app/src/main/scala/streaming/StreamingJob.scala
+++ app/src/main/scala/streaming/StreamingJob.scala
@@ -1,7 +1,7 @@
 package streaming
 
-import org.apache.spark.sql.SparkSession
 import org.apache.spark.sql.types.{LongType, StringType, StructField, StructType}
+import org.apache.spark.sql.{SQLContext, SparkSession}
 
 object StreamingJob {
   def main(args: Array[String]): Unit = {
@@ -9,8 +9,7 @@ object StreamingJob {
       .appName("Lambda with Spark")
       .master("local[*]")
       .getOrCreate()
-
-    val batchDuration = "2 seconds" //writestream.trigger(processingTime=...)
+    implicit val sqlContext: SQLContext = spark.sqlContext
 
     val inputtestSchema = StructType(Array(
       StructField("timestamp", LongType),
@@ -27,13 +26,40 @@ object StreamingJob {
       .option("delimiter", "\\t")
       .schema(inputtestSchema)
       .csv("C://Users/harry.crawshaw/Desktop/kafkaconnect/input")
+      .withWatermark("timestamp", "5 minutes")//
 
-    testDF.writeStream
-      .format("parquet")
-      .option("path", "C://Users/harry.crawshaw/Desktop/kafkaconnect/output")
-      .option("checkpointLocation", "C://Users/harry.crawshaw/Desktop/kafkaconnect/checkpoints")
+//    testDF.writeStream
+//      .format("console")
+//     // .trigger("processingTime", "4 seconds")
+//    //  .option("path", "C://Users/harry.crawshaw/Desktop/kafkaconnect/output")
+//     // .option("checkpointLocation", "C://Users/harry.crawshaw/Desktop/kafkaconnect/checkpoints")
+//      .start()
+//      .awaitTermination()
+
+//    val testing = spark.read.parquet("C://Users/harry.crawshaw/Desktop/kafkaconnect/output/tee1.snappy.parquet")
+//    testing.show()
+
+    testDF.createOrReplaceTempView("testDF")
+//
+  //  activityByProduct =
+    val sqlDF = spark.sql(
+      """
+        |SELECT
+        |product,
+        |timestamp,
+        |SUM(CASE WHEN action = 'purchase' THEN 1 ELSE 0 END) AS purchase_count,
+        |SUM(CASE WHEN action = 'add_to_cart' THEN 1 ELSE 0 END) AS add_to_cart_count,
+        |SUM(CASE WHEN action = 'page_view' THEN 1 ELSE 0 END) AS page_view_count
+        |FROM testDF
+        |GROUP BY product, timestamp
+        |""".stripMargin)
+
+
+    sqlDF.writeStream
+      .format("console")
       .start()
       .awaitTermination()
+
   }
 
 }
